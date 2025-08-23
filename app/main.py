80269da5728e01914afa368b4043d6be2dd85199
Sabina Gamidova <sabina.gamidova.dev@gmail.com>
2025-08-24 02:05:46 +0300

    Solution: resolved all previous conflicts

--- app/main.py
+++ app/main.py
@@ -1,1 +1,42 @@
-# write your code here
+from __future__ import annotations
+
+
+class AliveList(list):
+    def __repr__(self) -> str:
+        animals = ", ".join(
+            repr(animal) for animal in self
+        )
+        return f"[{animals}]"
+
+
+class Animal:
+    alive: list[Animal] = AliveList()
+
+    def __init__(
+            self,
+            name: str,
+            health: int = 100,
+            hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.__class__.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: Herbivore) -> None:
+        if not isinstance(herbivore, Carnivore) and not herbivore.hidden:
+            herbivore.health -= 50
+        if herbivore.health <= 0:
+            Animal.alive.remove(herbivore)
